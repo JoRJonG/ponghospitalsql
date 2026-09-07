eb5b2c42675ebb003b5773531d576bf9a70370dc
JoRJonG <ittiphol.jomg@gmail.com>
2026-09-07 09:48:53 +0700

    chore(sitemap): remove /it-center from static routes

--- server/cron/sitemapCron.js
+++ server/cron/sitemapCron.js
@@ -51,8 +51,7 @@ export async function generateSitemap() {
       { loc: `${siteUrl}/activities`, priority: '0.70', changefreq: 'weekly', lastmod: today },
       { loc: `${siteUrl}/documents`, priority: '0.75', changefreq: 'weekly', lastmod: today },
 
-      { loc: `${siteUrl}/air-quality`, priority: '0.80', changefreq: 'hourly', lastmod: today },
-      { loc: `${siteUrl}/it-center`, priority: '0.75', changefreq: 'monthly', lastmod: today }
+      { loc: `${siteUrl}/air-quality`, priority: '0.80', changefreq: 'hourly', lastmod: today }
     ]
 
     let dynamicUrls = []
